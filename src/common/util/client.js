d3247e791c48d52fb66bdfe24eb3bdce484457d6
Matt Huggins <matt.huggins@gmail.com>
2017-09-03 13:24:44 -0500

    Updated TODO with more meaningful comment

--- src/common/util/client.js
+++ src/common/util/client.js
@@ -1,4 +1,9 @@
 import * as neo from "neo-api-js";
 
-// TODO: fetch nodes from https://www.neoscan.io/api/main_net/v1/get_all_nodes
+// TODO: Calculate the best node to use.  Can model this after the approach used by Neon Wallet,
+// which exposes a "best node" enpoint at http://api.neonwallet.com/v2/network/best_node
+// The best node is based upon the current height & response time across all nodes (via
+// http://api.neonwallet.com/v2/network/nodes).  Source for `get_highest_node` can be found here:
+// https://github.com/CityOfZion/neon-wallet-db/blob/master/api/blockchain.py#L49
+
 export default neo.node("http://seed1.cityofzion.io:8080");
