2a877a09e60a0be96e8e7bc2b15ed8809075fc9d
Matt Huggins <matt.huggins@gmail.com>
2017-09-05 11:34:49 -0500

    Added redux-devtools middleware

--- src/common/boot/configureStore.js
+++ src/common/boot/configureStore.js
@@ -1,4 +1,5 @@
 import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import createSagaMiddleware from "redux-saga";
 
 import reducer from "../reducers";
@@ -6,7 +7,7 @@ import saga from "../sagas";
 
 export default function configureStore(state = {}) {
   const sagaMiddleware = createSagaMiddleware();
-  const store = createStore(reducer, state, applyMiddleware(sagaMiddleware));
+  const store = createStore(reducer, state, composeWithDevTools(applyMiddleware(sagaMiddleware)));
 
   if (module.hot) {
     module.hot.accept("../reducers", () => {
