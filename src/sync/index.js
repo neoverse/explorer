79e3269eb6c078001f5f89802278dc49ca14a5dd
Matt Huggins <matt.huggins@gmail.com>
2017-09-15 21:29:27 -0500

    Updated sync to handle concurrent requests

--- src/sync/index.js
+++ src/sync/index.js
@@ -1,67 +1,24 @@
-/* eslint-disable no-console */
+/* eslint-disable no-process-exit */
 
 import "babel-polyfill";
-import _ from "lodash";
-import * as neo from "neo-api-js";
 
-import database, { Block, Transaction } from "../server/database";
-import findBestNode from "./findBestNode";
+import Syncer from "./syncer";
+import database from "../server/database";
 
-const VERBOSE = 1;
-
-async function createBlock(block, options = {}) {
-  const attrs = _.pick(block, "hash", "index", "confirmations", "merkleroot", "nextconsensus",
-    "nonce", "previousblockhash", "script", "size", "time", "version");
-
-  return Block.create({ ...attrs, time: block.time * 1000 }, options);
-}
-
-async function createTransactions(transactions, block, options = {}) {
-  return Transaction.bulkCreate(transactions.map((tx) => {
-    const attrs = _.pick(tx, "txid", "type", "size", "nonce", "sys_fee", "net_fee", "scripts",
-      "version", "vin", "vout");
-
-    return { ...attrs, attrs: tx.attributes, blockhash: block.hash, blocktime: block.time * 1000 };
-  }), options);
-}
-
-async function getLastIndex() {
-  const lastBlock = await Block.findOne({ order: [["index", "desc"]] });
-  return lastBlock ? lastBlock.index : -1;
-}
+const syncer = new Syncer({
+  concurrency: Number(process.env.SYNC_CONCURRENCY || 5)
+});
 
 async function run() {
   await database.authenticate();
   await database.sync();  // TODO: remove this in favor of running a migration before the app starts
 
-  const node = await findBestNode();
-  const client = neo.node(node.url);
-
-  const lastIndex = await getLastIndex();
-  const blockHeight = await client.getBlockCount();
-
-  console.log(`Fetching blocks from ${lastIndex + 1} - ${blockHeight}`);
-
-  for (let index = lastIndex + 1; index < blockHeight; index++) {
-    let success = false;
-
-    while (!success) {
-      const transaction = await database.transaction();
-
-      try {
-        const block = await client.getBlockByHeight(index, VERBOSE);
-        console.log(`Saving block #${block.index} (${block.hash})`);
-        await createBlock(block, { transaction });
-        await createTransactions(block.tx, block, { transaction });
-        await transaction.commit();
-
-        success = true;
-      } catch (err) {
-        console.error(err);
-        await transaction.rollback();
-      }
-    }
-  }
+  syncer.start();
 }
+
+process.on("SIGINT", () => {
+  syncer.stop();
+  process.exit();
+});
 
 run();
---
+++ src/sync/syncer.js
@@ -0,0 +1,123 @@
+/* eslint-disable no-console */
+
+import _ from "lodash";
+import async from "async";
+import * as neo from "neo-api-js";
+
+import database, { Block, Transaction } from "../server/database";
+import findBestNode from "./findBestNode";
+
+const VERBOSE = 1;
+const PRIORITY_DEFAULT = 5;
+const PRIORITY_IMMEDIATE = 0;
+
+export default class Syncer {
+  constructor({ concurrency = 1, pollInterval = 1000, queueSize = 1000 } = {}) {
+    this.client = null;
+    this.pollInterval = pollInterval;
+    this.queueSize = queueSize;
+    this.queue = async.priorityQueue(this._processIndex, concurrency);
+    this.queue.drain = async () => { await this._poll(); };
+  }
+
+  start = async () => {
+    if (!this.client) {
+      this.client = await this._createClient();
+    }
+
+    await this._poll();
+  }
+
+  stop = () => {
+    if (this.clock) {
+      clearInterval(this.clock);
+      delete this.clock;
+    }
+  }
+
+  _createClient = async () => {
+    const node = await findBestNode();
+    return neo.node(node.url);
+  }
+
+  _poll = async () => {
+    await this._compareBlockHeight();
+    if (this.queue.length() > 0) return;
+
+    this.clock = setTimeout(async () => {
+      await this._compareBlockHeight();
+
+      if (this.queue.length() > 0) {
+        this.clearInterval(this.clock);
+      }
+    }, this.pollInterval);
+  }
+
+  _compareBlockHeight = async () => {
+    const height = await this.client.getBlockCount();
+    const nextIndex = await this._getNextIndex();
+    const maxIndex = nextIndex + Math.min(height - nextIndex, this.queueSize) - 1;
+
+    console.log(`Enqueueing fetches for blocks ${nextIndex} to ${maxIndex}...`);
+
+    for (let index = nextIndex; index <= maxIndex; index++) {
+      this._enqueue({ index, height });
+    }
+
+    console.log("Enqueued.");
+  }
+
+  _enqueue = ({ index, height }, priority = PRIORITY_DEFAULT) => {
+    if (index < height) {
+      this.queue.push({ index, height }, priority);
+    }
+  }
+
+  _retry = ({ index, height }) => {
+    this._enqueue({ index, height }, PRIORITY_IMMEDIATE);
+  }
+
+  _processIndex = async ({ index, height }, callback) => {
+    const transaction = await database.transaction();
+
+    try {
+      console.log(`Fetching block #${index}...`);
+
+      // TODO: if the below call fails, we should recreate the client with new best node
+      const block = await this.client.getBlockByHeight(index, VERBOSE);
+
+      await this._createBlock(block, { transaction });
+      await this._createTransactions(block.tx, block, { transaction });
+      await transaction.commit();
+
+      console.log(`Saved block #${block.index} (${block.hash})`);
+    } catch (err) {
+      console.error("Error saving block:", err);
+      await transaction.rollback();
+      this._retry({ index, height });
+    }
+
+    callback();
+  }
+
+  _getNextIndex = async () => {
+    const lastBlock = await Block.findOne({ order: [["index", "desc"]] });
+    return lastBlock ? lastBlock.index + 1 : 0;
+  }
+
+  _createBlock = async (block, options = {}) => {
+    const attrs = _.pick(block, "hash", "index", "confirmations", "merkleroot", "nextconsensus",
+      "nonce", "previousblockhash", "script", "size", "time", "version");
+
+    return Block.create({ ...attrs, time: block.time * 1000 }, options);
+  }
+
+  _createTransactions = async (transactions, block, options = {}) => {
+    return Transaction.bulkCreate(transactions.map((tx) => {
+      const attrs = _.pick(tx, "txid", "type", "size", "nonce", "sys_fee", "net_fee", "scripts",
+        "version", "vin", "vout");
+
+      return { ...attrs, attrs: tx.attributes, blockhash: block.hash, blocktime: block.time * 1000 };
+    }), options);
+  }
+}
