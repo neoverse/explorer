72543e194d53c4d69f3eac87c59af15949745a2c
Matt Huggins <matt.huggins@gmail.com>
2017-09-18 13:39:03 -0500

    Moved transaction constants into file

---
+++ src/common/values/transactions.js
@@ -0,0 +1,9 @@
+export const AGENCY_TRANSACTION = "AgencyTransaction";
+export const CLAIM_TRANSACTION = "ClaimTransaction";
+export const CONTRACT_TRANSACTION = "ContractTransaction";
+export const ENROLLMENT_TRANSACTION = "EnrollmentTransaction";
+export const INVOCATION_TRANSACTION = "InvocationTransaction";
+export const ISSUE_TRANSACTION = "IssueTransaction";
+export const MINER_TRANSACTION = "MinerTransaction";
+export const REGISTER_TRANSACTION = "RegisterTransaction";
+export const VOTING_TRANSACTION = "VotingTransaction";
