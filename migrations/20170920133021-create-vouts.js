2507874c8cdb7d6b85525682b514406cd6b57963
Matt Huggins <matt.huggins@gmail.com>
2017-09-20 08:56:38 -0500

    Added vouts

---
+++ migrations/20170920133021-create-vouts.js
@@ -0,0 +1,18 @@
+exports.up = (db, Sequelize) => {
+  return db.createTable("vouts", {
+    id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
+    txid: { type: Sequelize.STRING, allowNull: false },
+    address: { type: Sequelize.STRING, allowNull: false },
+    asset: { type: Sequelize.STRING, allowNull: false },
+    n: { type: Sequelize.INTEGER, allowNull: false },
+    value: { type: Sequelize.DECIMAL, allowNull: false },
+    created_at: { type: Sequelize.DATE, notNull: true },
+    updated_at: { type: Sequelize.DATE, notNull: true }
+  }).then(() => {
+    return db.addIndex("vouts", ["txid"]);
+  });
+};
+
+exports.down = (db, Sequelize) => {
+  return db.dropTable("vouts");
+};
