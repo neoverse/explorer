c19f21ea140e360ef002c267327375b919afce80
Matt Huggins <matt.huggins@gmail.com>
2017-09-30 08:24:05 -0400

    Updated home page to match adjusted content area padding

--- src/common/components/home.js
+++ src/common/components/home.js
@@ -1,9 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default class Home extends React.Component {
   static displayName = "Home";
 
   render = () => {
-    return <div>Welcome</div>;
+    return (
+      <div className="home-component">
+        <h1>Welcome</h1>
+        <p>
+          Coming soon: interesting charts about blocks, transactions, addresses, etc.
+        </p>
+        <p>
+          For now, explore the latest <Link to="/blocks">blocks</Link> and{" "}
+          <Link to="/transactions">transactions</Link>.
+        </p>
+      </div>
+    );
   }
 }
