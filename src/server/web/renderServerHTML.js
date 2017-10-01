3b396bc300542546714f60a07113c7dd1dc57717
Matt Huggins <matt.huggins@gmail.com>
2017-10-01 08:10:57 -0500

    Added google site verification

--- src/server/web/renderServerHTML.js
+++ src/server/web/renderServerHTML.js
@@ -20,10 +20,11 @@ export default function renderServerHTML({ html = "", state = {}, title = defaul
     <!DOCTYPE html>
     <html>
       <head>
-        <meta charset="utf-8">
+        <meta charset="utf-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="msapplication-tap-highlight" content="no">
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="msapplication-tap-highlight" content="no" />
+        <meta name="google-site-verification" content="WnQTWW4Ou5zNc0BIsYEmVeTQXp4s05SGkf4JwoAizbs" />
         <title>${title}</title>
         <link rel="shortcut icon" type="image/png" href="/favicon.ico" />
         <script async src="https://www.googletagmanager.com/gtag/js?id=UA-107269744-1"></script>
