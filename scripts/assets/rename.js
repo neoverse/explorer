578f2cf0252d6f45b814fc541ab3fd512738ba42
Matt Huggins <matt.huggins@gmail.com>
2017-10-08 11:42:55 -0500

    Added constant values for NEO/GAS txid's

--- scripts/assets/rename.js
+++ scripts/assets/rename.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-process-exit, no-console */
 
 import db, { Asset } from "../../src/server/database";
+import { NEO_ID, GAS_ID } from "../../src/common/values/assets";
 
 async function execute() {
   const transaction = await db.transaction();
@@ -9,14 +10,14 @@ async function execute() {
     await Asset.update({
       name: [{ name: "小蚁股", lang: "zh-CN" }, { name: "NEO", lang: "en" }]
     }, {
-      where: { txid: "c56f33fc6ecfcd0c225c4ab356fee59390af8560be0e930faebe74a6daff7c9b" },
+      where: { txid: NEO_ID },
       transaction
     });
 
     await Asset.update({
       name: [{ name: "小蚁币", lang: "zh-CN" }, { name: "GAS", lang: "en" }]
     }, {
-      where: { txid: "602c79718b16e442de58778e148d0b1084e3b2dffd5de6b7b16cee7969282de7" },
+      where: { txid: GAS_ID },
       transaction
     });
 
