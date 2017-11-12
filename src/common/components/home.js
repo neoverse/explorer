05720c68e1f32e261dcdc274bc82ed9aeae44932
Matt Huggins <matt.huggins@gmail.com>
2017-11-12 09:48:49 -0600

    Updated home route to redirect to blocks

--- src/common/components/home.js
+++ src/common/components/home.js
@@ -1,21 +1,19 @@
 import React from "react";
-import { Link } from "react-router-dom";
+
+import historyShape from "../shapes/historyShape";
 
 export default class Home extends React.Component {
   static displayName = "Home";
 
+  static propTypes = {
+    history: historyShape.isRequired
+  };
+
+  componentDidMount = () => {
+    this.props.history.replace("/blocks");
+  }
+
   render = () => {
-    return (
-      <div className="home-component">
-        <h2>Welcome</h2>
-        <p>
-          Coming soon: interesting charts about blocks, transactions, addresses, etc.
-        </p>
-        <p>
-          For now, explore the latest <Link to="/blocks">blocks</Link> and{" "}
-          <Link to="/transactions">transactions</Link>.
-        </p>
-      </div>
-    );
+    return null;
   }
 }
