fc9560bbd7c11798346b7ba3c74279e6aeca5782
Matt Huggins <matt.huggins@gmail.com>
2017-11-12 20:15:54 -0600

    Removed ambiguous search loading/failed components

--- src/common/containers/search.js
+++ src/common/containers/search.js
@@ -4,8 +4,8 @@ import { compose, setDisplayName } from "recompose";
 import withGraphQuery from "../hocs/graphql/withGraphQuery";
 import withGraphProgress from "../hocs/graphql/withGraphProgress";
 import Search from "../components/search/search";
-import Loading from "../components/search/loading";
-import Failed from "../components/search/failed";
+import Loading from "../components/loading";
+import Failed from "../components/failed";
 import NoMatches from "../components/search/noMatches";
 
 const query = gql`
